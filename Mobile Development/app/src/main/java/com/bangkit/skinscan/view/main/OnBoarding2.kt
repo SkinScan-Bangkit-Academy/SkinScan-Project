ea6d0a0ab0e2658eaa1e36eaf2101ab388ac3ae3
yasgiyas <hanifghiyatsramadhan@gmail.com>
2024-06-07 22:49:03 +0700

    change layout onboarding and apply animation add image on drawable add color

--- Mobile Development/app/src/main/java/com/bangkit/skinscan/view/main/OnBoarding2.kt
+++ Mobile Development/app/src/main/java/com/bangkit/skinscan/view/main/OnBoarding2.kt
@@ -12,10 +12,5 @@ class OnBoarding2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_on_boarding2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
