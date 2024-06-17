9dc0bdfcd2c3ae33482f9551d9b11b191624f5ee
yasgiyas <hanifghiyatsramadhan@gmail.com>
2024-06-17 16:21:46 +0700

    change the change password into delete account

--- Mobile Development/app/src/main/java/com/bangkit/skinscan/fragment/profile/DeleteAccount.kt
+++ Mobile Development/app/src/main/java/com/bangkit/skinscan/fragment/profile/DeleteAccount.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bangkit.skinscan.R
 
-class ChangePassword : AppCompatActivity() {
+class DeleteAccount : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_change_password)
+        setContentView(R.layout.activity_delete_account)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
